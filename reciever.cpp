247e0b9314bc008ef6675879e05f93143e065668
michael bishop <clever@nbnet.nb.ca>
2012-06-21 15:03:51 -0300

    fix varint parsing and add a ton of other parsing

--- reciever.cpp
+++ reciever.cpp
@@ -33,28 +33,40 @@ struct protobuf_packet {
 	} data;
 	unsigned int bytes_used;
 };
+struct varint {
+	int bytes_used;
+	int number;
+};
+struct varint read_varint(unsigned char *buffer,int size) {
+	struct varint t;
+	unsigned char temp;
+	temp = *buffer;
+	int final_number = temp & 0x7f;
+	t.bytes_used = 1;
+	int offset = 0;
+	while (temp & 0x80) {
+		offset++;
+		buffer++;
+		temp = *buffer;
+		final_number += (temp & 0x7f) << (7 * offset);
+		//printf("1: %u, 2: %u, 3: %u\n",final_number,temp,offset);
+		t.bytes_used++;
+	}
+	t.number = final_number;
+	return t;
+}
 struct protobuf_packet parse_packet(unsigned char *buffer,int size) {
 	struct protobuf_packet t;
 	t.type = buffer[0] & 7;
 	t.field = buffer[0] >> 3;
 	unsigned char *pointer = buffer+1;
-	unsigned char temp;
-	printf("%02x field %u type %u\n",buffer[0],t.field,t.type);
+	//printf("%02x field %u type %u\n",buffer[0],t.field,t.type);
 	switch (t.type) {
 		case 0: {
-			temp = *pointer;
-			int final_number = temp & 0x7f;
-			t.bytes_used = 2;
-			int offset = 0;
-			while (temp & 0x80) {
-				offset++;
-				pointer++;
-				temp = *pointer;
-				final_number += temp << (7 * offset);
-				//printf("1: %u, 2: %u, 3: %u\n",final_number,temp,offset);
-				t.bytes_used++;
-			}
-			t.data.number = final_number;
+			t.bytes_used = 1; // tag
+			struct varint x = read_varint(pointer,size-1);
+			t.bytes_used += x.bytes_used;
+			t.data.number = x.number;
 			break;
 			}
 		case 1:
@@ -62,13 +74,13 @@ struct protobuf_packet parse_packet(unsigned char *buffer,int size) {
 			t.bytes_used = 9;
 			break;
 		case 2: {
-			int size2 = buffer[1];
-			assert((size2 & 0x80) == 0); // doesnt handle varint right
-			assert((size2+1) <= size);
+			struct varint x = read_varint(pointer,size-1);
+			int size2 = x.number;
 			t.data.blob.data = new unsigned char[size2];
 			t.data.blob.size = size2;
-			memcpy(t.data.blob.data,buffer+1,size2);
-			t.bytes_used = 2 + size2;
+			t.bytes_used = 1 + x.bytes_used + size2;
+			assert(t.bytes_used <= size);
+			memcpy(t.data.blob.data,buffer+2,size2);
 			break;
 			}
 		case 5:
@@ -145,19 +157,89 @@ void hex_spew(unsigned char *buffer,int size) {
 class rpc_state {
 public:
 	rpc_state() {
-		bindToken = connectToken = 0;
+		logonToken = bind2Token = bind1Token = connectToken = -1;
 	}
 	void setConnectToken(int token) {
 		connectToken = token;
 	}
-	void setBindToken(int token) {
-		bindToken = token;
-	}
+	void setBind1Token(int token) { bind1Token = token; }
+	void setBind2Token(int token) { bind2Token = token; }
 	enum target { CLIENT,SERVER };
 	void reply(enum target t,int token,int size,unsigned char *buffer) {
 		if ((t == CLIENT) && (token == connectToken)) {
 			handleConnectResponse(size,buffer);
+		} else if ((t == CLIENT) && (token == bind1Token)) {
+			handleBindResponse(size,buffer,0);
+		} else if ((t == CLIENT) && (token == bind2Token)) {
+			handleBindResponse(size,buffer,1);
+		} else if (t == CLIENT) {
+			if (token == logonToken) {} // empty reply
 		}
+	}
+	void handleLogon(unsigned char *buffer,int size,int token) {
+		logonToken = token;
+		unsigned char *x = buffer;
+		int size_left = size;
+		do {
+			struct protobuf_packet p;
+			p = parse_packet(x,size_left);
+			x += p.bytes_used;
+			size_left -= p.bytes_used;
+			switch (p.field) {
+			case 1:
+				assert(p.type == 2);
+				assert(p.data.blob.size == 2);
+				assert(p.data.blob.data[0] == 'D');
+				assert(p.data.blob.data[1] == '3'); // ensure this is D3
+				delete p.data.blob.data;
+				break;
+			case 4:
+				assert(p.type == 2);
+				printf("email %s\n",p.data.blob.data);
+				delete p.data.blob.data;
+				break;
+			case 5:
+				assert(p.type == 2);
+				printf("client version %s\n",p.data.blob.data);
+				delete p.data.blob.data;
+				break;
+			default:
+				printf("field %u type %u\n",p.field,p.type);
+			}
+		} while (size_left > 0);
+		assert(size_left == 0);
+	}
+	void handleBindResponse(int size,unsigned char *buffer,int mode) {
+		unsigned char *x = buffer;
+		int size_left = size;
+		do {
+			struct protobuf_packet p;
+			p = parse_packet(x,size_left);
+			x += p.bytes_used;
+			size_left -= p.bytes_used;
+			switch (p.field) {
+			case 1: {
+				assert(p.type == 2);
+				if (mode == 0) {
+					assert(p.data.blob.size == 2);
+					unsigned char id1,id2;
+					id1 = p.data.blob.data[0];
+					id2 = p.data.blob.data[1];
+					printf("bnet.protocol.channel.Channel %u\nbnet.protocol.presence.PresenceService %u\n",id1,id2);
+				} else if (mode == 1) {
+					assert(p.data.blob.size == 1);
+					unsigned char id;
+					id = p.data.blob.data[0];
+					printf("bnet.protocol.authentication.AuthenticationServer %u\n",id);
+				} else assert(0);
+				delete p.data.blob.data;
+				break;
+				}
+			default:
+				printf("field %u type %u\n",p.field,p.type);
+			}
+		} while (size_left > 0);
+		assert(size_left == 0);
 	}
 	void handleConnectResponse(int size,unsigned char *buffer) {
 		unsigned char *x = buffer;
@@ -170,14 +252,14 @@ public:
 			switch (p.field) {
 			case 1:
 				assert(p.type == 2);
-				printf("ConnectResponse.ProcessId == ");
-				hex_spew(p.data.blob.data,p.data.blob.size);
+				printf("ConnectResponse.server_id == ");
+				dump_packet(p.data.blob.data,p.data.blob.size);
 				delete p.data.blob.data;
 				break;
 			case 2:
 				assert(p.type == 2);
 				printf("ConnectResponse.client_id == ");
-				hex_spew(p.data.blob.data,p.data.blob.size);
+				dump_packet(p.data.blob.data,p.data.blob.size);
 				delete p.data.blob.data;
 				break;
 			case 5:
@@ -192,8 +274,32 @@ public:
 		} while (size_left > 0);
 		assert(size_left == 0);
 	}
+	void dump_packet(unsigned char *buf,int size) {
+		if (size == 0) return;
+		unsigned char *x = buf;
+		int size_left = size;
+		do {
+			struct protobuf_packet p;
+			p = parse_packet(x,size_left);
+			x += p.bytes_used;
+			size_left -= p.bytes_used;
+			switch (p.type) {
+			case 0:
+				printf("field %u type %u == %u\n",p.field,p.type,p.data.number);
+				break;
+			case 2:
+				printf("field %u type %u == ",p.field,p.type);
+				hex_spew(p.data.blob.data,p.data.blob.size);
+				delete p.data.blob.data;
+				break;
+			default:
+				printf("field %u type %u unhandled\n",p.field,p.type);
+			}
+		} while (size_left > 0);
+		assert(size_left == 0);
+	}
 private:
-	int connectToken,bindToken;
+	int connectToken,bind1Token,bind2Token,logonToken;
 };
 class rpc_decoder {
 public:
@@ -229,6 +335,10 @@ public:
 					assert(p.type == 0);
 					SizeTag = p.data.number;
 					break;
+				case 6:
+					assert(p.type == 0);
+					if (p.data.number) printf("status %u\n",p.data.number);
+					break;
 				default:
 					switch (p.type) {
 						case 0:
@@ -246,7 +356,7 @@ public:
 			assert(SizeTag <= stream.size());
 			stream.read(buffer,SizeTag);
 			header = buffer;
-			for (;header < (buffer + SizeTag);header++) {
+			/*for (;header < (buffer + SizeTag);header++) {
 				if (*header < 0x20) printf("_");
 				else if (*header > 0x80) printf("=");
 				else printf("%c",*header);
@@ -257,7 +367,7 @@ public:
 				t = buffer[i];
 				printf("%02x ",t);
 			}
-			printf("\n");
+			printf("\n");*/
 			handle_rpc(ServiceId,MethodId,Token,SizeTag,buffer);
 			delete buffer;
 		} else {
@@ -276,15 +386,25 @@ public:
 private:
 	void handle_rpc(int service,int method,int token,int size,unsigned char *buffer) {
 		switch (service) {
-		case 1:
-			printf("CLIENT: AuthenticationClient Method %u, data %u, token %u\n",method,size,token);
+		case 2:
+			switch (method) {
+			case 1:
+				printf("CLIENT: AuthenticationClient::ModuleLoad token %u data %u\n",token,size);
+				state->dump_packet(buffer,size);
+				break;
+			default:
+				printf("CLIENT: AuthenticationClient Method %u, data %u, token %u\n",method,size,token);
+				state->dump_packet(buffer,size);
+			}
 			break;
 		case 254:
 			printf("CLIENT: reply for token %u, data %u\n",token,size);
+			state->dump_packet(buffer,size);
 			state->reply(rpc_state::CLIENT,token,size,buffer);
 			break;
 		default:
 			printf("CLIENT: unknown service %u, method %u, data %u, token %u\n",service,method,size,token);
+			state->dump_packet(buffer,size);
 		}
 	}
 };
@@ -302,21 +422,77 @@ private:
 				printf("SERVER: ConnectionService::Connect, data %u, token %u\n",size,token);
 				state->setConnectToken(token);
 				break;
-			case 2:
+			case 2: {
 				printf("SERVER: ConnectionService::Bind, data %u, token %u\n",size,token);
-				state->setBindToken(token);
+				unsigned char *x = buffer;
+				int size_left = size;
+				do {
+					struct protobuf_packet p;
+					p = parse_packet(x,size_left);
+					x += p.bytes_used;
+					size_left -= p.bytes_used;
+					switch (p.field) {
+					case 1: {
+						assert(p.type == 2);
+						if (p.data.blob.size == 8) {
+							unsigned int serviceid1,serviceid2; // must be 32bit
+							serviceid1 = *((unsigned int*)p.data.blob.data);
+							serviceid2 = *((unsigned int*)(p.data.blob.data+4));
+							printf("requesting %x and %x from server\n",serviceid1,serviceid2);
+							assert (serviceid1 == 0xb732db32); //bnet.protocol.channel.Channel 
+							assert(serviceid2 == 0xfa0796ff); //bnet.protocol.presence.PresenceService 
+							state->setBind1Token(token);
+						} else if (p.data.blob.size == 4) {
+							unsigned int serviceid1; // must be 32bit
+							serviceid1 = *((unsigned int*)p.data.blob.data);
+							printf("requesting %x from server\n",serviceid1);
+							assert(serviceid1 == 0x0decfc01); //bnet.protocol.authentication.AuthenticationServer 
+							state->setBind2Token(token);
+						} else assert(0);
+						break;
+						}
+					case 2: {
+						assert(p.type == 2);
+						assert(p.data.blob.size == 7);
+						// it contains a second protobuf, but i'm cheating here
+						assert(p.data.blob.data[0] == 0x0d);
+						assert(p.data.blob.data[5] == 0x10);
+						unsigned int serviceid = *((unsigned int*) (p.data.blob.data+1));
+						unsigned char service = *((unsigned char*) (p.data.blob.data+6));
+						printf("exporting service %x as %u\n",serviceid,service);
+						if (serviceid == 0xbf8c8094) { //bnet.protocol.channel.ChannelSubscriber 
+							printf("bnet.protocol.channel.ChannelSubscriber == %u\n",service);
+						} else if (serviceid == 0x71240e35) { // bnet.protocol.authentication.AuthenticationClient
+							printf("bnet.protocol.authentication.AuthenticationClient == %u\n",service);
+						} else assert(0);
+						break;
+						}
+					}
+				} while (size_left > 0);
+				assert(size_left == 0);
 				break;
+				}
 			default:
 				printf("SERVER: ConnectionService method %u, data %u, token %u\n",method,size,token);
+				state->dump_packet(buffer,size);
 			}
 			break;
 		case 1:
+			// module message has srp6a info?
 			switch (method) {
 			case 1:
 				printf("SERVER: AuthenticationServer::Logon, data %u, token %u\n",size,token);
+				state->handleLogon(buffer,size,token);
+				break;
+			case 2:
+				printf("SERVER: AuthenticationServer::ModuleNotify, data %u, token %u\n",size,token);
+				hex_spew(buffer,size);
+				state->dump_packet(buffer,size);
 				break;
 			default:
 				printf("SERVER: AuthenticationServer method %u\n",method);
+				hex_spew(buffer,size);
+				state->dump_packet(buffer,size);
 			}
 			break;
 		default:
@@ -378,7 +554,7 @@ int main(int argc, char *argv[]) {
 				t = buffer[i];
 				printf("%02x ",t);
 			}*/
-			printf("\n\n");
+			printf("\n");
 		}
 	}
 	printf("done\n");
